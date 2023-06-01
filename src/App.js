9a458ecbaa39e2d05672f8549c1e8bd716479908
AthulShaji <athulshaji475@gmail.com>
2023-06-01 13:09:39 +0530

    initial commit of appicatiion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+const handleSubmite=(e)=>{
+  e.preventDefault();
+  const formdata=new FormData()
+  formdata.append('file',e.target.file.files[0])//appending file data as file
+  const plainFormData = Object.fromEntries(formdata.entries());
+console.log(plainFormData);
+
+  axios.post('http://localhost:5001/ExctractTextFromimage', formdata)
+  .then(response => {
+    // Handle the response data
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmite}>
+    <label>UploadImage:</label>
+        <input type='file' name='file'></input>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
